04d35a8814159f9607c81f5156bcdb45973a1be6
BRUNONOMAR\Bruno <bmarquesn@gmail.com>
2021-08-19 16:37:55 -0300

    HCode Futebol Club Components

---
+++ 11_components/hcode-futebol-clube-12-components/src/components/HcodeSection.vue
@@ -0,0 +1,59 @@
+<template>
+
+    <div>
+        
+        <div class="container">
+
+            <div class="row mt-5">
+                <h3>Você está vendo notícias do: {{ championship }}</h3>
+            </div>
+
+        </div>
+        
+        <component :is="currentComponent"></component>
+
+        <div class="container">
+
+            <div class="row my-club mt-5">
+
+                <div class="col-6">
+                    <h2>Seu clube é: {{ myClub }}</h2>
+                </div>
+
+                <div class="col-6">
+                    <HcodeInput v-model="myClub" />
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import HcodeSectionBanner from './HcodeSectionBanner'
+import HcodeInput from './HcodeInput'
+
+export default {
+    components: {
+        HcodeSectionBanner,
+        HcodeSectionNews: () => import('./HcodeSectionNews'),
+        HcodeInput
+    },
+    data() {
+        return {
+            myClub: 'Hcode Treinamentos'
+        }
+    },
+    props: {
+        championship: String,
+        currentComponent: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
