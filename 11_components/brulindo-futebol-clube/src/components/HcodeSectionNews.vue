ea6bbf67e3e2ede6d74d1a8bc59806083db1b27c
BRUNONOMAR\Bruno <bmarquesn@gmail.com>
2021-08-19 14:37:39 -0300

    Conteúdo dinâmico em Components utilizando " props "

--- 11_components/brulindo-futebol-clube/src/components/HcodeSectionNews.vue
+++ 11_components/brulindo-futebol-clube/src/components/HcodeSectionNews.vue
@@ -2,9 +2,27 @@
     <section>
         <div class="container">
             <!-- <HcodeSectionNewsIndividual /> -->
-            <hcode-section-news-individual />
-            <hcode-section-news-individual />
-            <hcode-section-news-individual />
+            <hcode-section-news-individual
+                img-name="news1.jpg"
+                img-info="Notícia 1"
+                news-title="O SPFC é o melhor time!!!"
+                news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut earum, necessitatibus, maxime, sit obcaecati eius reiciendis fugiat porro aliquam dolores ipsam mollitia sed vero at a omnis alias illo amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita praesentium ea accusantium ipsa ex asperiores officia itaque vitae quidem necessitatibus quam a, amet, quod non error laudantium deleniti. Vel, a.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident itaque quidem ex quod blanditiis deserunt, ullam mollitia tenetur impedit quibusdam odit possimus praesentium quia iste at laboriosam. Nam, vitae."
+                news-date="10/01/2020"
+             />
+            <hcode-section-news-individual
+                img-name="news2.jpg"
+                img-info="Notícia 2"
+                news-title="Começam os Treinos para a Nova Temporada"
+                news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut earum, necessitatibus, maxime, sit obcaecati eius reiciendis fugiat porro aliquam dolores ipsam mollitia sed vero at a omnis alias illo amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita praesentium ea accusantium ipsa ex asperiores officia itaque vitae quidem necessitatibus quam a, amet, quod non error laudantium deleniti. Vel, a.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident itaque quidem ex quod blanditiis deserunt, ullam mollitia tenetur impedit quibusdam odit possimus praesentium quia iste at laboriosam. Nam, vitae."
+                news-date="20/01/2020"
+            />
+            <hcode-section-news-individual
+                img-name="news3.jpg"
+                img-info="Notícia 3"
+                news-title="Um, Dois, Três Pim"
+                news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut earum, necessitatibus, maxime, sit obcaecati eius reiciendis fugiat porro aliquam dolores ipsam mollitia sed vero at a omnis alias illo amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita praesentium ea accusantium ipsa ex asperiores officia itaque vitae quidem necessitatibus quam a, amet, quod non error laudantium deleniti. Vel, a.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident itaque quidem ex quod blanditiis deserunt, ullam mollitia tenetur impedit quibusdam odit possimus praesentium quia iste at laboriosam. Nam, vitae."
+                news-date="30/01/2020"
+            />
         </div>
     </section>
 </template>
--- 11_components/brulindo-futebol-clube/src/components/HcodeSectionNewsIndividual.vue
+++ 11_components/brulindo-futebol-clube/src/components/HcodeSectionNewsIndividual.vue
@@ -1,19 +1,40 @@
 <template>
     <div class="row">
         <div class="col-3">
-            <img src="../assets/news1.jpg" alt="Notícia 1" />
+            <img :src="require('../assets/' + this.imgName)" :alt="imgInfo" />
         </div>
         <div class="col-9">
-            <h2>Começam os Treinos para a Nova Temporada</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut earum, necessitatibus, maxime, sit obcaecati eius reiciendis fugiat porro aliquam dolores ipsam mollitia sed vero at a omnis alias illo amet!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita praesentium ea accusantium ipsa ex asperiores officia itaque vitae quidem necessitatibus quam a, amet, quod non error laudantium deleniti. Vel, a.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident itaque quidem ex quod blanditiis deserunt, ullam mollitia tenetur impedit quibusdam odit possimus praesentium quia iste at laboriosam. Nam, vitae.</p>
-            <span class="font-italic">01/01/2020</span>
+            <h2>{{ newsTitle }}</h2>
+            <p>{{ newsContent | truncate(200) }}</p>
+            <span class="font-italic">{{ newsDate }}</span>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        imgName: {
+            type: String,
+            required: true
+        },
+        imgInfo: {
+            type: String,
+            required: true
+        },
+        newsTitle: {
+            type: String,
+            required: true
+        },
+        newsContent: {
+            type: String,
+            required: true
+        },
+        newsDate: {
+            type: String,
+            required: true
+        }
+    }
 }
 </script>
 
